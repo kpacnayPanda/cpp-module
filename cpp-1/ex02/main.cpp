c1f2efe5a07ccfeba9b2525764d4e1f804835dcf
Morrowind Rosette <mrosette@oa-l1.msk.21-school.ru>
2021-09-18 11:18:08 +0300

    cpp1 ex02

--- cpp-1/ex02/main.cpp
+++ cpp-1/ex02/main.cpp
@@ -6,7 +6,12 @@ int main(void) {
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
-	std::cout << "ptr: " << *stringPTR << std::endl;
-	std::cout << "ref: " << &stringREF << std::endl;
+	std::cout << "adresses:" << std::endl;
+	std::cout << "string " << &str << std::endl;
+	std::cout << "ptr " << stringPTR << std::endl;
+	std::cout << "ref " << &stringREF << std::endl;
+	std::cout << "\nstrings:" << std::endl;
+	std::cout << "ptr " << *stringPTR << std::endl;
+	std::cout << "ref " << stringREF << std::endl;
 	return (0);
 }
